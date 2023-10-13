851439b3223e0df929ba227920657699a45343c3
Hermes Alves <hermes@softagon.com.br>
2023-10-13 06:39:39 -0300

    feat: add vue-chartjs

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,48 @@
 <script setup lang="ts">
 console.log("Fruits");
 const ogImageOptions = {
-  title: 'My awesome home page.',
-}
+  title: "My awesome home page.",
+};
 </script>
 <template>
   <v-container>
     <SBreadcrumb>
-    <template #breadcrumb="{ to, title }">
-      <NuxtLink :to="to">
-        {{ title }}
-      </NuxtLink>
-    </template>
-  </SBreadcrumb>
-  <OgImage v-bind="ogImageOptions" />
-    Choose one
-    <NuxtLink to="/banana">Banana</NuxtLink>
-    <NuxtLink to="https://twitter.com/nuxt_js" target="_blank">
-      Nuxt Twitter
-    </NuxtLink>
-    <!-- <a href="https://twitter.com/nuxt_js" target="_blank" rel="noopener noreferrer">...</a> -->
-
-    <NuxtLink to="https://discord.nuxtjs.org" target="_blank" rel="noopener">
-      Nuxt Discord
-    </NuxtLink>
-    <!-- <a href="https://discord.nuxtjs.org" target="_blank" rel="noopener">...</a> -->
-
-    <NuxtLink to="https://github.com/nuxt" no-rel> Nuxt GitHub </NuxtLink>
-    <!-- <a href="https://github.com/nuxt">...</a> -->
-
-    <NuxtLink to="/contact" target="_blank">
-      Contact page opens in another tab
-    </NuxtLink>
+      <template #breadcrumb="{ to, title }">
+        <NuxtLink :to="to">
+          {{ title }}
+        </NuxtLink>
+      </template>
+    </SBreadcrumb>
+    <OgImage v-bind="ogImageOptions" />
+    <ul>
+      <ol>
+        <NuxtLink to="/pie">Pie</NuxtLink>
+      </ol>
+      <ol>
+        <NuxtLink to="/banana">Banana</NuxtLink>
+      </ol>
+      <ol>
+        <NuxtLink to="https://twitter.com/nuxt_js" target="_blank">
+          Nuxt Twitter
+        </NuxtLink>
+      </ol>
+      <ol>
+        <NuxtLink
+          to="https://discord.nuxtjs.org"
+          target="_blank"
+          rel="noopener"
+        >
+          Nuxt Discord
+        </NuxtLink>
+      </ol>
+      <ol>
+        <NuxtLink to="https://github.com/nuxt" no-rel> Nuxt GitHub </NuxtLink>
+      </ol>
+      <ol>
+        <NuxtLink to="/contact" target="_blank">
+          Contact page opens in another tab
+        </NuxtLink>
+      </ol>
+    </ul>
   </v-container>
 </template>
---
+++ pages/pie.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-container>
+    Pie chart sample
+    <Pie
+      :chart-options="chartOptions"
+      :data="chartData"
+    />
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { Pie } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
+} from "chart.js";
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+
+const chartData = ref({
+  labels: [
+    "Basic Materials",
+    "Consumer Cyclical",
+    "Financial Services",
+    "Real Estate",
+    "Consumer Defensive",
+    "Healthcare",
+    "Utilities",
+    "Communication Services",
+    "Energy",
+    "Industrials",
+    "Technology",
+  ],
+  datasets: [
+    {
+      backgroundColor: [
+        "#FF4A4A",
+        "#FFAC4A",
+        "#FFE9C9",
+        "#F9C87C",
+        "#F97432",
+        "#7a7979",
+        "#FFCC00",
+        "#FF9900",
+        "#86370e",
+        "#FFFF66",
+        "#ed9e67",
+      ],
+      data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    },
+    {
+      backgroundColor: [
+        "#FF4A4A",
+        "#FFAC4A",
+        "#FFE9C9",
+        "#F9C87C",
+        "#F97432",
+        "#7a7979",
+        "#FFCC00",
+        "#FF9900",
+        "#86370e",
+        "#FFFF66",
+        "#ed9e67",
+      ],
+      data: [1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1],
+    },
+  ],
+});
+const chartOptions = ref({
+  responsive: true,
+  maintainAspectRatio: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+});
+
+//methods
+const loadData = () => {};
+
+//add on mount API request
+onMounted(() => {
+  loadData();
+});
+</script>
+
+<style scoped>
+#pie-chart {
+  width: 30% !important;
+  height: 30%;
+}
+</style>
